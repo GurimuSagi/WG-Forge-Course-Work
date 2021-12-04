d794af02fd0fee7a9a6d6991891e820690a74a28
Nikolay13-11 <mikola13.11@mail.ru>
2021-12-04 21:45:51 +0300

    add basic function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './style/style.scss';
+import './services/router/router';
+import './services/api/api';
+import './services/app/app';
---
+++ src/services/router/router.js
@@ -0,0 +1,38 @@
+import data from '../helper/data';
+import routes from './routes';
+
+const grid = document.querySelector('.grid');
+
+window.location.hash = '#/';
+let id;
+
+// eslint-disable-next-line no-restricted-globals
+const parseLocation = () => location.hash.slice(1).toLowerCase() || '/';
+
+const router = () => {
+    const detailTank = data.find((tank) => tank.tank_id === Number(id));
+    const path = parseLocation();
+    const componentByPath = (p, r) => r.find((item) => item.path === p) || undefined;
+    const { component } = componentByPath(path, routes);
+    if (path === '/') {
+        setTimeout(() => {
+            grid.style.display = 'grid';
+            grid.innerHTML = component.render(data);
+        }, 2000);
+    } else if (path === '/detail') {
+        grid.innerHTML = component.render(detailTank);
+        grid.style.display = 'block';
+    } else if (path === '/wishlist') {
+        grid.innerHTML = component.render(data);
+        grid.style.display = 'grid';
+    }
+};
+
+grid.addEventListener('click', (event) => {
+    if (!event.target.classList.contains('checkbox')) {
+        window.location.hash = '/detail';
+        id = (event.target.closest('article')).dataset.id;
+    }
+});
+
+export default router;
