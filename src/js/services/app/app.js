2a2748c7021d03eb730dd5949fddd44167d5ddc0
Nikolay13-11 <mikola13.11@mail.ru>
2021-12-14 22:13:29 +0300

    start create shipping cart

--- src/js/services/app/app.js
+++ src/js/services/app/app.js
@@ -1,3 +1,6 @@
+import { coverPlace, shoppingButton } from '../helper/constants';
+import { closeShoppingCart, openShoppingCart } from '../helper/core';
+
 import router from '../router/router';
 
 const premium = document.querySelector('.premium');
@@ -8,3 +11,6 @@ window.addEventListener('load', router);
 premium.addEventListener('click', () => {
     window.location.hash = '/';
 });
+
+shoppingButton.addEventListener('click', openShoppingCart);
+coverPlace.addEventListener('click', closeShoppingCart);
